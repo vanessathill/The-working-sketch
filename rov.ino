4591272925f1f0e9067ef908757c9585dfdd22d2
vanessathill <129029736+vanessathill@users.noreply.github.com>
2023-03-27 16:01:56 +1100

    Working 2 motors

--- rov.ino
+++ rov.ino
@@ -1,16 +1,18 @@
 #include <Servo.h>
-Servo ESC;     // create servo object to control the ESC
-int test = 1250;
+Servo ESC1;     // create servo object to control the ESC
+Servo ESC2;     // create servo object to control the ESC
 
 void setup() {
   Serial.begin(9600);
-  ESC.attach(6,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC1.attach(6,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC2.attach(5,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
   delay(6000);
 }
 
 void loop() {
-   for (int i = 1000; i < 1300; i++) {
-    ESC.writeMicroseconds(i);
+   for (int i = 1000; i < 1050; i++) {
+    ESC1.writeMicroseconds(i);
+    ESC2.writeMicroseconds(i);
     /*
     if (i> 1250)  {
       delay(1000); // halt motor for 1 second
@@ -25,7 +27,8 @@ void loop() {
   // Stalling the loop by entering an infinite while loop
   int check = 0;
   // During this stall the speed is 0
-  ESC.writeMicroseconds(0);
+  ESC1.writeMicroseconds(0);
+  ESC2.writeMicroseconds(0);
   while (check == 0) {
   
   }
