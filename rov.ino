710d0f77d8402986ffc2c3e2efa7692d8dcf77a3
vanessathill <129029736+vanessathill@users.noreply.github.com>
2023-04-13 10:30:59 +1000

    New_pooltest1_sketch

--- rov.ino
+++ rov.ino
@@ -2,7 +2,9 @@
 Servo ESC1;     // create servo object to control the ESC
 Servo ESC2;     // create servo object to control the ESC
 
-int change_speed(int current_speed_m1, int new_speed, Servo ESC_number);
+int change_speed_m1(int current_speed, int new_speed);
+int change_speed_m2(int current_speed, int new_speed);
+
 int motorpin_1 = 6;
 int motorpin_2 = 5;
 int stop_speed = 1000;
@@ -22,38 +24,53 @@ void loop() {
   if (Serial.available() > 0) {
     char command = Serial.read();
     if (command == 'f') {
-      current_speed_m1 = change_speed(current_speed_m1, default_speed, ESC1);
-      current_speed_m2 = change_speed(current_speed_m2, default_speed, ESC2);
+      current_speed_m1 = change_speed_m1(current_speed_m1, default_speed);
+      current_speed_m2 = change_speed_m2(current_speed_m2, default_speed);
 
     } else if (command == 'l') {
-      current_speed_m1 = change_speed(current_speed_m1, stop_speed, ESC1);
-      current_speed_m2 = change_speed(current_speed_m2, default_speed, ESC2);
+      current_speed_m1 = change_speed_m1(current_speed_m1, stop_speed);
+      current_speed_m2 = change_speed_m2(current_speed_m2, default_speed);
 
     } else if (command == 'r') {
-      current_speed_m1 = change_speed(current_speed_m1, default_speed, ESC1);
-      current_speed_m2 = change_speed(current_speed_m2, stop_speed, ESC2);
+      current_speed_m1 = change_speed_m1(current_speed_m1, default_speed);
+      current_speed_m2 = change_speed_m2(current_speed_m2, stop_speed);
     
     } else if (command == 's') {
-      current_speed_m1 = change_speed(current_speed_m1, stop_speed, ESC1);
-      current_speed_m2 = change_speed(current_speed_m2, stop_speed, ESC2);
+      current_speed_m1 = change_speed_m1(current_speed_m1, stop_speed);
+      current_speed_m2 = change_speed_m2(current_speed_m2, stop_speed);
     }
   }
-
-  Serial.println(ESC1.read());
-
 }
 
-int change_speed(int current_speed, int new_speed, Servo ESC_number) {
+int change_speed_m1(int current_speed, int new_speed) {
 
   if (current_speed < new_speed) {
     for (int i = current_speed; i < new_speed; i ++) {
-      ESC_number.writeMicroseconds(i);
+      ESC1.writeMicroseconds(i);
       delay(10);
     }
 
   } else {
     for (int i = current_speed; i > new_speed; i --) {
-      ESC_number.writeMicroseconds(i);
+      ESC1.writeMicroseconds(i);
+      delay(10);
+    }
+  }
+
+  return new_speed;
+}
+
+int change_speed_m2(int current_speed, int new_speed) {
+
+  if (current_speed < new_speed) {
+    for (int i = current_speed; i < new_speed; i ++) {
+      ESC2.writeMicroseconds(i);
+      delay(10);
+    }
+
+  } else {
+    for (int i = current_speed; i > new_speed; i --) {
+      ESC2.writeMicroseconds(i);
       delay(10);
     }
   }
