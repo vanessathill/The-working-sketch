5ffc59f456c7b33d16f70c72769cb9645f5a6388
someretical <29365738+someretical@users.noreply.github.com>
2023-04-17 15:36:12 +1000

    Update rov2.ino

--- rov2.ino
+++ rov2.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-#define LEFT_PIN 9
+#define LEFT_PIN 5
 #define RIGHT_PIN 10
 #define VERT_PIN 11
 #define CLAW_PIN 6
@@ -17,6 +17,9 @@
 #define MIN_PULSE 1000
 #define MAX_PULSE 2000
 
+#define MOTOR_PULSE_STEP 15
+#define SERVO_PULSE_STEP 3
+
 Servo s_left;
 Servo s_right;
 Servo s_vert;
@@ -30,9 +33,13 @@ int left_goal = MIN_PULSE;
 int right_goal = MIN_PULSE;
 int vert_goal = MIN_PULSE;
 
-bool claw_open = true;
+#define CLAW_OPEN 60
+#define CLAW_CLOSED 135
+int claw_goal = CLAW_OPEN;
+int claw_pulse = CLAW_OPEN;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   pinMode(JOY1_HOR, INPUT);
@@ -50,45 +57,62 @@ void setup() {
   delay(5000);
 }
 
-void loop() {
+void loop()
+{
   int y1 = map(analogRead(JOY1_VER), 0, 1023, -512, 511);
   int x1 = map(analogRead(JOY1_HOR), 0, 1023, -512, 511);
   int y2 = map(analogRead(JOY2_VER), 0, 1023, -512, 511);
   int x2 = map(analogRead(JOY2_HOR), 0, 1023, -512, 511);
 
-  bool left_press = digitalRead(LEFT_SEL) == LOW;
-  bool right_press = digitalRead(RIGHT_SEL) == LOW;
+  bool left_press = digitalRead(LEFT_SEL) == HIGH;
+  bool right_press = digitalRead(RIGHT_SEL) == HIGH;
 
   // Update claw
-  // if (left_press && !claw_open)
-  //   claw_open = true;
-  // else if (right_press && claw_open)
-  //   claw_open = false;
+  if (left_press && claw_goal == CLAW_CLOSED)
+  {
+    claw_goal = CLAW_OPEN;
+  }
+  else if (right_press && claw_goal == CLAW_OPEN)
+  {
+    claw_goal = CLAW_CLOSED;
+  }
 
-  // // Update claw
-  // if (claw_open) {
-  //   s_claw.write(0);
-  // } else {
-  //   s_claw.write(180);
-  // }
+  if (claw_goal > claw_pulse)
+  {
+    claw_pulse += SERVO_PULSE_STEP;
+  }
+  else if (claw_goal < claw_pulse)
+  {
+    claw_pulse -= SERVO_PULSE_STEP;
+  }
+  s_claw.write(claw_pulse);
 
   // Update goals
 
-  // Joystick 
-  if (x1 < 99 && x1 > -100) {
-    if (y1 < 99) {
+  // Joystick
+  if (x1 < 99 && x1 > -100)
+  {
+    if (y1 < 99)
+    {
       // Dead zone
       left_goal = MIN_PULSE;
       right_goal = MIN_PULSE;
-    } else {
+    }
+    else
+    {
       left_goal = map(y1, 0, 511, MIN_PULSE, MAX_PULSE);
       right_goal = map(y1, 0, 511, MIN_PULSE, MAX_PULSE);
     }
-  } else {
-    if (x1 < 0) {
+  }
+  else
+  {
+    if (x1 < 0)
+    {
       right_goal = map(x1, -1, -512, MIN_PULSE, MAX_PULSE);
       left_goal = MIN_PULSE;
-    } else {
+    }
+    else
+    {
       left_goal = map(x1, 0, 511, MIN_PULSE, MAX_PULSE);
       right_goal = MIN_PULSE;
     }
@@ -97,63 +121,71 @@ void loop() {
   /// Joystick 2
 
   // top
-  if (y2 > 50) {
+  if (y2 > 50)
+  {
     vert_goal = map(y2, 0, 511, MIN_PULSE, MAX_PULSE);
   }
   // bottom
-  else {
+  else
+  {
     vert_goal = MIN_PULSE;
   }
 
   // Update motors themselves
-  // Serial.print("XY: ");
-  // Serial.print(x1);
-  // Serial.print(" ");
-  // Serial.print(y1);
-  // Serial.print(" | ");
+  Serial.print("XY: ");
+  Serial.print(x1);
+  Serial.print(" ");
+  Serial.print(y1);
+  Serial.print(" | ");
 
-  // Serial.print("L: ");
-  // Serial.print(left_goal);
-  // Serial.print(" ");
-  // Serial.print(left_pulse);
-  // Serial.print(" | ");
+  Serial.print("L: ");
+  Serial.print(left_goal);
+  Serial.print(" ");
+  Serial.print(left_pulse);
+  Serial.print(" | ");
 
-  // Serial.print("R: ");
-  // Serial.print(right_goal);
-  // Serial.print(" ");
-  // Serial.print(right_pulse);
-  // Serial.print(" | ");
+  Serial.print("R: ");
+  Serial.print(right_goal);
+  Serial.print(" ");
+  Serial.print(right_pulse);
+  Serial.print(" | ");
 
-  // Serial.print("V: ");
-  // Serial.print(vert_goal);
-  // Serial.print(" ");
-  // Serial.print(vert_pulse);
-  // Serial.print("\n");
+  Serial.print("V: ");
+  Serial.print(vert_goal);
+  Serial.print(" ");
+  Serial.print(vert_pulse);
+  Serial.print("\n");
 
-
-  if (left_goal > left_pulse) {
-    left_pulse += 2;
-    s_left.writeMicroseconds(left_pulse);
-  } else if (left_goal < left_pulse) {
-    left_pulse -= 2;
-    s_left.writeMicroseconds(left_pulse);
+  if (left_goal > left_pulse)
+  {
+    left_pulse += MOTOR_PULSE_STEP;
+  }
+  else if (left_goal < left_pulse)
+  {
+    left_pulse -= MOTOR_PULSE_STEP;
   }
 
-  if (right_goal > right_pulse) {
-    right_pulse += 2;
-    s_right.writeMicroseconds(right_pulse);
-  } else if (right_goal < right_pulse) {
-    right_pulse -= 2;
-    s_right.writeMicroseconds(right_pulse);
+  if (right_goal > right_pulse)
+  {
+    right_pulse += MOTOR_PULSE_STEP;
+  }
+  else if (right_goal < right_pulse)
+  {
+    right_pulse -= MOTOR_PULSE_STEP;
   }
 
-  if (vert_goal > vert_pulse) {
-    vert_pulse += 2;
-    s_vert.writeMicroseconds(vert_pulse);
-  } else if (vert_goal < vert_pulse) {
-    vert_pulse -= 2;
-    s_vert.writeMicroseconds(vert_pulse);
+  if (vert_goal > vert_pulse)
+  {
+    vert_pulse += MOTOR_PULSE_STEP;
+  }
+  else if (vert_goal < vert_pulse)
+  {
+    vert_pulse -= MOTOR_PULSE_STEP;
   }
 
-  delay(2);
+  s_right.writeMicroseconds(right_pulse);
+  s_left.writeMicroseconds(left_pulse);
+  s_vert.writeMicroseconds(vert_pulse);
+
+  delay(15);
 }
